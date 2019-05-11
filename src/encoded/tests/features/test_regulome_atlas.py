40f788ff2e91ddffdafe368720728d76a2bbe97d
Ben Hitz <hitz@stanford.edu>
2019-05-10 17:27:38 -0700

    test find_peaks

--- src/encoded/tests/features/test_regulome_atlas.py
+++ src/encoded/tests/features/test_regulome_atlas.py
@@ -7,14 +7,88 @@ import pytest
 
 pytestmark = [pytest.mark.indexing]
 
+
+@pytest.fixture(scope='function')
+def regulome_atlas(registry):
+    from encoded.regulome_atlas import RegulomeAtlas
+    from snovault.elasticsearch import interfaces # NOQA
+    return RegulomeAtlas(registry[interfaces.SNP_SEARCH_ES])
+
+
 @pytest.mark.parametrize("assembly,location,snps", [
     ('hg19', ('chr1', 39492462, 39492462), ['rs3768324']),
     ('hg19', ('chr10', 5894500, 5894500), ['rs10905307']),
 ])
-def test_find_snps(assembly, location, snps, region_index, testapp, registry):
-    from encoded.regulome_atlas import RegulomeAtlas
-    from snovault.elasticsearch import interfaces # NOQA
-    atlas = RegulomeAtlas(registry[interfaces.SNP_SEARCH_ES])
+def test_find_snps(assembly, location, snps, region_index, regulome_atlas):
     found = [snp['rsid'] for snp in
-             atlas.find_snps(assembly, location[0], location[1], location[2])]
+             regulome_atlas.find_snps(assembly, location[0], location[1], location[2])]
     assert found == snps
+
+
+@pytest.mark.parametrize("assembly,location,dbpeaks", [
+    ('hg19', ('chr10', 5894500, 5894500), [
+        {'coordinates': {'gte': 5894433, 'lte': 5894748},
+         'uuid': '956cba28-ccff-4cbd-b1c8-39db4e3de572'},
+        {'coordinates': {'gte': 5894500, 'lte': 5894500},
+         'uuid': '5f921aa5-5758-4ead-846a-26af87e1a098'}
+    ]),
+])
+def test_find_peaks(assembly, location, dbpeaks, region_index, regulome_atlas):
+    peaks = regulome_atlas.find_peaks(assembly, location[0], location[1], location[2])
+
+    assert len(peaks) == 2
+    foundpeaks = [p['_source'] for p in peaks]
+    for dbp in dbpeaks:
+        assert dbp in foundpeaks
+
+@pytest.mark.parametrize("assembly,location,dbdetails", [
+    ('hg19', ('chr10', 5894500, 5894500), [
+        {'dataset': 
+            {   '@id': '/annotations/ENCSR061TST/',
+                'annotation_type': 'dsQTLs',
+                'collection_type': 'dsQTLs',
+                'uuid': '4109b15f-8bf7-4711-b644-43032f5c27e0'
+            },
+         'file': 
+            {   '@id': '/files/ENCFF122TST/',
+                'assembly': 'hg19',
+                'uuid': '5f921aa5-5758-4ead-846a-26af87e1a098'
+            },
+        },
+        {'dataset':
+            {   '@id': '/experiments/ENCSR000DZQ/',
+                'assay_term_name': 'ChIP-seq',
+                'biosample_term_name': 'GM12878',
+                'collection_type': 'ChIP-seq',
+                'target': ['EBF1'],
+                'uuid': 'd9161058-d8c4-4b17-b03b-bfaeabe75e02'
+          },
+         'file': {   '@id': '/files/ENCFF002COS/',
+                     'assembly': 'hg19',
+                     'uuid': '956cba28-ccff-4cbd-b1c8-39db4e3de572'
+            },
+        },
+    ]),
+])
+def test_find_peaks_filtered(assembly, location, dbdetails, region_index, regulome_atlas):
+    fpeaks, details = regulome_atlas.find_peaks_filtered(assembly, location[0], location[1], location[2])
+    for part in ('dataset', 'file'): 
+        assert fpeaks[0]['resident_detail'][part] == dbdetails[0][part] \
+            or fpeaks[0]['resident_detail'][part] == dbdetails[1][part]
+        assert fpeaks[1]['resident_detail'][part] == dbdetails[0][part] \
+            or fpeaks[1]['resident_detail'][part] == dbdetails[1][part]
+
+def test_resident_details():
+    pass
+
+
+def details_breakdown():
+    pass
+
+
+def test_regulome_evidence():
+    pass
+
+
+def test_nearby_snps():
+    pass
