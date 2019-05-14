8e660814d1b5cd7ffa3dcb4513d72e9df7882d8c
Ben Hitz <hitz@stanford.edu>
2019-05-13 17:24:39 -0700

    add nearby_snps test, one doesn't work

--- src/encoded/regulome_atlas.py
+++ src/encoded/regulome_atlas.py
@@ -66,6 +66,7 @@ class RegulomeAtlas(object):
         '''Return single SNP by rsid and assembly'''
         try:
             res = self.region_es.get(index=snp_index_key(assembly), doc_type='_all', id=rsid)
+            #TODO _all is deprecated
         except Exception:
             return None
 
@@ -77,14 +78,29 @@ class RegulomeAtlas(object):
         # get all peaks that overlap requested point
         # only single point intersection
         # use start not end for 0-base open ended
-        query = {
-            'query': {
-                'term': {
-                    'coordinates': start
-                }
-            },
-            'size': max_results,
-        }
+        if start == end:
+            query = {
+                'query': {
+                    'term': {
+                        'coordinates': start
+                    }
+                },
+                'size': max_results,
+            }
+        else:
+            query = {
+                'query': {
+                    'range': {
+                        'coordinates': {
+                            'gte': start,
+                            'lte': end,
+                            'relation': 'within',
+                        }
+                    }
+                },
+                'size': max_results,
+            }
+
 
         return query
 
@@ -161,12 +177,10 @@ class RegulomeAtlas(object):
 
         overlap = set()
         for peak in peaks:
-            for hit in peak['_source']['coordinates']:
-                if chrom == peak['_index'] and \
-                      start <= hit['lte'] and \
-                      end >= hit['gte']:
-                    overlap.add(peak['_source']['uuid'])
-                    break
+            if chrom == peak['_index'] and \
+                    start <= peak['_source']['coordinates']['lte'] and \
+                    end >= peak['_source']['coordinates']['gte']:
+                overlap.add(peak['_source']['uuid'])
 
         return overlap
 
@@ -390,10 +404,10 @@ class RegulomeAtlas(object):
         if len(snps) <= window:
             return snps
 
-        snps = sorted(snps, key=itemgetter('start', 'end', 'rsid'))
+        snps = sorted(snps, key=lambda s: s['coordinates']['start'])
         ix = 0
         for snp in snps:
-            if snp['start'] >= center_pos:
+            if snp['coordinates']['gte'] >= center_pos:
                 break
             ix += 1
 
@@ -410,8 +424,8 @@ class RegulomeAtlas(object):
         if window > 0:
             snps = self._snp_window(snps, window, center_pos)
 
-        start = snps[0]['start']  # SNPs must be in location order!
-        end = snps[-1]['end']                                        # MUST do SLOW peaks_too
+        start = snps[0]['coordinates']['gte']  # SNPs must be in location order!
+        end = snps[-1]['coordinates']['lte']                                        # MUST do SLOW peaks_too
         (peaks, details) = self.find_peaks_filtered(assembly, chrom, start, end, peaks_too=True)
         if not peaks or not details:
             for snp in snps:
@@ -424,7 +438,7 @@ class RegulomeAtlas(object):
         for snp in snps:
             snp['score'] = None  # default
             snp['assembly'] = assembly
-            snp_uuids = self._peak_uuids_in_overlap(peaks, snp['chrom'], snp['start'])
+            snp_uuids = self._peak_uuids_in_overlap(peaks, snp['chrom'], snp['coordinates']['gte'])
             if snp_uuids:
                 if snp_uuids == last_uuids:  # good chance evidence hasn't changed
                     if last_snp:
