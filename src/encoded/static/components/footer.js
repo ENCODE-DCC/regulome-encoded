20880e03c4b727e8d18cec2eae9fe88d6fe662a6
Emma O'Neill <emmaruthoneill@gmail.com>
2019-09-23 16:06:22 -0700

    REG-145 updates to beta website

--- src/encoded/static/components/footer.js
+++ src/encoded/static/components/footer.js
@@ -27,10 +27,8 @@ const Footer = ({ version }, reactContext) => {
                             <ul className="footer-links">
                                 <li><a href="https://www.stanford.edu/site/privacy/">Privacy</a></li>
                                 <li><a href="mailto:regulomedb@mailman.stanford.edu">Contact</a></li>
-                                <li><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
                                 <li id="user-actions-footer">{userActionRender}</li>
                             </ul>
-                            <p className="copy-notice">&copy;{new Date().getFullYear()} Stanford University.</p>
                         </div>
 
                         <div className="col-sm-6 col-sm-pull-6">
@@ -38,6 +36,7 @@ const Footer = ({ version }, reactContext) => {
                                 <li><a href="/regulome-search"><img src="/static/img/RegulomeLogoFinal_mixedcolors.png" alt="Regulome" id="encode-logo" height="35px" width="120px" /></a></li>
                                 <li><a href="http://www.stanford.edu"><img src="/static/img/su-logo-white-2x.png" alt="Stanford University" id="su-logo" width="105px" height="49px" /></a></li>
                                 <li><a href="https://umich.edu/"><img src="/static/img/umich-logo-blue.svg" alt="University of Michigan" id="umich-logo" width="75px" height="81px" /></a></li>
+                                <li><a href="https://creativecommons.org/licenses/by/4.0/"><img src="/static/img/creative-commons-logo.png" alt="Creative Commons" id="cc-logo" /></a></li>
                             </ul>
                         </div>
                         <p className="copy-notice">&copy;{new Date().getFullYear()} Stanford University</p>
