373a0656ea4f6c890c5c5c0861cfe612110c26e7
Emma O'Neill <emmaruthoneill@gmail.com>
2020-03-20 10:30:08 -0700

    subtle style

--- src/encoded/static/components/regulome_summary.js
+++ src/encoded/static/components/regulome_summary.js
@@ -80,8 +80,10 @@ const RegulomeSummary = (props) => {
     return (
         <React.Fragment>
             <div className="lead-logo">
-                <a href="/"><img src="/static/img/RegulomeLogoFinal.gif" alt="Regulome logo" /></a>
-                <div className="version-tag">2.0</div>
+                <a href="/">
+                    <img src="/static/img/RegulomeLogoFinal.gif" alt="Regulome logo" />
+                    <div className="version-tag">2.0</div>
+                </a>
             </div>
 
             <div className="notification-label-centered">
